2c891d32696c87630a20b9bacab5b5e982e2e9f6
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-06-22 11:07:23 +0530

    Update the database relationship between the Batch and eligible Students  .

--- src/models/index.ts
+++ src/models/index.ts
@@ -2,6 +2,8 @@
 import Faculty from "./faculty.model";
 import Department from "./department.model";
 import User from "./user.model";
+import Batch from "./batch.model";
+import EligibleStudent from "./eligibleStudent.model";
 
 Faculty.hasMany(Department, { foreignKey: "facultyId" });
 Department.belongsTo(Faculty, { foreignKey: "facultyId" });
@@ -12,4 +14,7 @@ User.belongsTo(Faculty, { foreignKey: "facultyId" });
 Department.hasMany(User, { foreignKey: "departmentId" });
 User.belongsTo(Department, { foreignKey: "departmentId" });
 
-export { Faculty, Department, User };
+Batch.hasMany(EligibleStudent, {foreignKey: "batchId",});
+EligibleStudent.belongsTo(Batch, {foreignKey: "batchId",});
+
+export { Faculty, Department, User, Batch, EligibleStudent };
