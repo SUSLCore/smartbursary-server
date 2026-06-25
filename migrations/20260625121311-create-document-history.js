c6818b015421ad6ab66a9812765290eb7f8e3b0b
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-06-25 19:07:46 +0530

    Update the DB migrations

---
+++ migrations/20260625121311-create-document-history.js
@@ -0,0 +1,76 @@
+"use strict";
+
+const DOCUMENT_STEPS = [
+  "FACULTY_MA_UPLOAD",
+  "SAR_APPROVAL",
+  "FACULTY_ASR_APPROVAL",
+  "DEPARTMENT_HEAD_APPROVAL",
+  "DEPARTMENT_MA_APPROVAL",
+  "DEPARTMENT_HEAD_RETURN",
+  "FACULTY_ASR_RETURN",
+  "SAR_RETURN",
+  "FACULTY_MA_FINAL",
+];
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("document_history", {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+
+      documentId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "monthly_documents",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+
+      uploadedBy: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE",
+      },
+
+      step: {
+        type: Sequelize.ENUM(...DOCUMENT_STEPS),
+        allowNull: false,
+      },
+
+      filePath: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      remarks: {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      },
+
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable("document_history");
+  },
+};
