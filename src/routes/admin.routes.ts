9617f3c0b63c6658254391a2b819cc85d60e2fd3
binu2002 <binojmadhuranga2002.04.04@gmail.com>
2026-07-09 21:58:15 +0530

    Add routes for managing monthly documents in admin panel

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { AdminController } from "../controllers/admin.controller";
+import { AdminMonthlyDocumentController } from "../controllers/adminMonthlyDocument.controller";
 import { protect } from "../middlewares/auth.middleware";
 import { authorize } from "../middlewares/authorize.middleware";
 import { UserRole } from "../types/user.types";
@@ -18,6 +19,21 @@ router.delete(
   protect,
   authorize(UserRole.ADMIN),
   AdminController.deleteUserByRegisterId
+);
+
+router.get(
+    "/monthly-documents",
+    protect,
+    authorize(UserRole.ADMIN),
+    AdminMonthlyDocumentController.getAllMonthlyDocuments
+);
+
+
+router.delete(
+    "/monthly-documents/:id",
+    protect,
+    authorize(UserRole.ADMIN),
+    AdminMonthlyDocumentController.deleteMonthlyDocument
 );
 
 export default router;
